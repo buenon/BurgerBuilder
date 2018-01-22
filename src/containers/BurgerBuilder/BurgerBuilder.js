1d2fcfd9b5c5c771ce89e040e97b278ebccd8325
Nadav Bueno <buenonadav@gmail.com>
2018-01-22 15:51:21 +0200

    Send purchase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxi';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from "../../axios-orders";
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const LIMIT_INGREDIENTS = false;
 
@@ -38,16 +40,17 @@ class BurgerBuilder extends Component {
         totalPrice: 4,
         purchasable: false,
         showSummary: false,
+        loading: false,
     };
 
     updatePurchasableState(ingredients) {
         let ingCount = Object.keys(ingredients).map((key) => {
             return ingredients[key].count;
-        }).reduce((sum,el) => {
+        }).reduce((sum, el) => {
             return sum + el;
         });
 
-        this.setState({purchasable: ingCount > 0});
+        this.setState({ purchasable: ingCount > 0 });
     }
 
     addIngredientHandler = (ingredient) => {
@@ -59,7 +62,7 @@ class BurgerBuilder extends Component {
         if (!LIMIT_INGREDIENTS || ing.count < ing.max) {
             ing.count += 1;
             let newPrice = this.state.totalPrice + ing.price;
-            this.setState({ingredients: curr, totalPrice: newPrice});
+            this.setState({ ingredients: curr, totalPrice: newPrice });
         }
 
         this.updatePurchasableState(curr);
@@ -74,22 +77,40 @@ class BurgerBuilder extends Component {
         if (ing.count > 0) {
             ing.count -= 1;
             let newPrice = this.state.totalPrice - ing.price;
-            this.setState({ingredients: curr, totalPrice: newPrice});
+            this.setState({ ingredients: curr, totalPrice: newPrice });
         }
 
         this.updatePurchasableState(curr);
     }
 
     purchaseHandler = () => {
-        this.setState({showSummary: true});
+        this.setState({ showSummary: true });
     }
 
     purchaseCancelHandler = () => {
-        this.setState({showSummary: false});
+        this.setState({ showSummary: false });
     }
 
     purchaseContinueHandler = () => {
-        alert("Not implemented");
+        this.setState({ loading: true });
+        axios.post('/orders.json', {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: "Nadav Bueno",
+                address: {
+                    street: "My street",
+                    zip: "12345",
+                    country: "Israel"
+                },
+                email: "test@nadav.com",
+            },
+            deliveryMethod: "fastest"
+        }).then(res => {
+            this.setState({ loading: false, showSummary: false });
+        }).catch(error => {
+            this.setState({ loading: false, showSummary: false });
+        });
     }
 
     render() {
@@ -103,16 +124,21 @@ class BurgerBuilder extends Component {
             };
         }
 
+        let modalContent = <OrderSummary
+            ingredients={this.state.ingredients}
+            cancel={this.purchaseCancelHandler}
+            continue={this.purchaseContinueHandler}
+            price={this.state.totalPrice} />
+        if (this.state.loading) {
+            modalContent = <Spinner />
+        }
+
         return (
             <Aux>
                 <Modal show={this.state.showSummary} close={this.purchaseCancelHandler}>
-                    <OrderSummary 
-                        ingredients={this.state.ingredients} 
-                        cancel={this.purchaseCancelHandler} 
-                        continue={this.purchaseContinueHandler} 
-                        price={this.state.totalPrice} />
+                    {modalContent}
                 </Modal>
-                <Burger ingredients={this.state.ingredients}/>
+                <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredients={this.state.ingredients}
                     add={this.addIngredientHandler}
